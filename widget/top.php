3bb8a2319e21fb73ac0c64d583865bf4e302721d
uyentrang247 <ienchang247@gmail.com>
2025-11-22 16:31:09 +0700

    style: update login and register page styling

--- widget/top.php
+++ widget/top.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LeaderClub</title>
-    <link rel="stylesheet" href="assets/css/trangchu.css">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="stylesheet" href="assets/css/register.css">
-    <link rel="stylesheet" href="assets/css/DanhsachCLB.css">
+    <link rel="stylesheet" href="assets/css/global.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.0.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 <body>
